282365ed43e3943e5ded58d2aba8d07503eb61df
DaffaNazhirul <102272906+DaffaNazhirul@users.noreply.github.com>
2022-07-29 17:10:57 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+let apiUrl = 'https://daffa07.deppp.my.id/index.php/';
+Vue.createApp({
+  data() {
+    return {
+      massage: 'Hi',
+      header: {},
+      articles: [],
+      article: null,
+    };
+  },
+  methods: {
+    getHeaderData() {
+      axios
+        .get(apiUrl + 'users/1')
+        .then((res) => {
+          this.header = res.data;
+          this.getArticles();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    getPortoData() {
+      axios
+        .get(apiUrl + 'portofolio/1')
+        .then((res) => {
+          this.porto = res.data;
+          this.getArticles();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    getArticles() {
+      axios
+        .get(apiUrl + 'article')
+        .then((res) => {
+          this.articles = res.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    getMarkdown() {
+      const queryString = window.location.search;
+      const urlParams = new URLSearchParams(queryString);
+      const articles = urlParams.get('article');
+      var converter = new showdown.Converter();
+      console.log(articles);
+      axios
+        .get((src = 'https://raw.githubusercontent.com/DaffaNazhirul/tekweb2022/main/contents/' + articles))
+        .then((res) => {
+          var html = converter.makeHtml(res.data);
+          this.article = html;
+          console.log(html);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+
+  beforeMount() {
+    this.getHeaderData(), this.getArticles(), this.getMarkdown(), this.getPortoData();
+  },
+}).mount('#app');
